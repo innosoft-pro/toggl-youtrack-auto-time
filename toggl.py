e98bf9a7240c6c31e56a22bdc3b40d087f38be9d
Aleksandr Shepelev <a.shepelev@innosoft.pro>
2017-04-23 11:06:25 +0500

    remove shebangs

--- toggl.py
+++ toggl.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- encoding: utf-8 -*-
 from configuration import TogglConfig, YoutrackConfig
 import requests
 import json
--- youtrack.py
+++ youtrack.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- encoding: utf-8 -*-
 from configuration import YoutrackConfig
 import requests
 from lxml import etree
